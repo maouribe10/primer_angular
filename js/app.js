38c77711ca7f9b57eef9b371f026bb4fd8925204
Mauricio Uribe <maouribe1020@gmail.com>
2016-04-20 23:17:28 -0500

    agregando controlador Tab para mostrar los paneles o pestañas

--- js/app.js
+++ js/app.js
@@ -5,9 +5,20 @@
 		this.products = gems;
 	});
 
+	app.controller('TabController', function(){
+		this.tab = 0;
+		this.setTab = function(setTab){
+			this.tab = setTab;
+		};
+		this. isTab = function(tab){
+			return this.tab === tab;
+		}
+	});
+
 	var gems = [
-		{name: 'dodecahedro', price: 3.50, description: 'esto es una gema muy grande y muy bonita, y dispone de mucho brillo y color', puedeComprarse: false}, 
-		{name: 'pentahedro', price: 6.30, description: 'Esta gema es muy pesada por lo que tiene un precio exhorbitante', puedeComprarse: true}
+		{name: 'Dodecahedro', price: 3.50, description: 'esto es una gema muy grande y muy bonita, y dispone de mucho brillo y color', puedeComprarse: false},
+		{name: 'Octahedro', price: 8.50, description: 'Una gema con clase y distinción que requiere ser portada por alguién que la merezca', puedeComprarse: true}, 
+		{name: 'Pentahedro', price: 6.30, description: 'Esta gema es muy pesada por lo que tiene un precio exhorbitante', puedeComprarse: true}
 	];
 
 })();
